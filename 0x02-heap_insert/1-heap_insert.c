159960400f66dd78f1f2267857887fca4a8662c9
andres0191 <andres_felipe0191@hotmail.com>
2021-02-11 17:51:10 -0500

    update task 1

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -10,5 +10,55 @@
 
 heap_t *heap_insert(heap_t **root, int value)
 {
+heap_t *node, *parent;
+	if (!*root)
+	{
+		node = binary_tree_node(*root, value);
+		*root = node;
+		return (*root);
+	}
+	node = *root;
 
+	while (node)
+	{
+		parent = node;
+		if (node->n == value)
+			return node_swap(node);
+		if (value > node->n)
+			node = parent->left;
+		else
+			node = node->right;
+	}
+
+	if (value > parent->n)
+	{
+		parent->left = binary_tree_node(parent, value);
+		return (parent->left);
+	}
+	else
+	{
+		parent->right = binary_tree_node(parent, value);
+		return (parent->right);
+	}
+}
+
+/*
+ * node_swap - Swaps node with larger node
+ * @node: Pointer
+ * Return: The new node
+ */
+
+heap_t *node_swap(heap_t *node)
+{
+	while (node && node->parent)
+	{
+		while (node->n > node->parent->n)
+		{
+			node = node->parent;
+			node->n += node->parent->n;
+			node->parent->n -= node->n;
+			node->parent->n = node->n - node->parent->n;
+		}
+	}
+	return (node);
 }
