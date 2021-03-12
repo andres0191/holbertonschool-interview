e379db17b060cb51f0eba9eae91da4c1a33168f8
andres0191 <andres_felipe0191@hotmail.com>
2021-03-11 22:00:32 -0500

    update task

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -16,39 +16,50 @@
     status codes should be printed in ascending order
 """
 
-if __name__ == '__main__':
+import sys
 
-    def printer(size, d):
-        """Print function"""
-        sort = sorted(d.keys())
-        print("File size: {:d}".format(size))
-        for i in sort:
-            if d[i] != 0:
-                print("{}: {}".format(i, d[i]))
+if __name__ == "__main__":
 
-    size = 0
-    d = {"200": 0, "301": 0, "400": 0, "401": 0,
-         "403": 0, "404": 0, "405": 0, "500": 0}
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
+    file_size = [0]
+    count = 1
 
-    counter = 0
+    def print_stats():
+        '''
+        Prints file size and stats for every 10 loops
+        '''
+        print('File size: {}'.format(file_size[0]))
+
+        for code in sorted(status_codes.keys()):
+            if status_codes[code] != 0:
+                print('{}: {}'.format(code, status_codes[code]))
+
+    def parse_stdin(line):
+        '''
+        Checks the stdin for matches
+        '''
+        try:
+            line = line[:-1]
+            word = line.split(' ')
+            # File size is last parameter on stdout
+            file_size[0] += int(word[-1])
+            # Status code comes before file size
+            status_code = int(word[-2])
+            # Move through dictionary of status codes
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
+
     try:
-        with open(0) as f:
-            for line in f:
-                counter += 1
-                arr = line.split()
-                try:
-                    size += int(arr[-1])
-                except:
-                    pass
-                try:
-                    st = arr[-2]
-                    if st in d:
-                        d[st] += 1
-                except:
-                    pass
-                if counter % 10 == 0:
-                    printer(size, d)
-            printer(size, d)
+        for line in sys.stdin:
+            parse_stdin(line)
+            # print the stats after every 10 outputs
+            if count % 10 == 0:
+                print_stats()
+            count += 1
     except KeyboardInterrupt:
-        printer(size, d)
+        print_stats()
         raise
+    print_stats()
