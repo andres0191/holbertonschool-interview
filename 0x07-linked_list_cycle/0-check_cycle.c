c27d7e43d3957bbe4dbbd8912c6168096874fec4
andres0191 <andres_felipe0191@hotmail.com>
2021-03-17 22:07:46 -0500

    add second version of project

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -1,36 +1,30 @@
 #include "lists.h"
 
 /**
- * check_cycle - Check if a singly linked list is a cycle
- * @list: The pointer to the list's head
- * Return: 0 if has no cycle, 1 if any other case
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ * @list: The pointer to the head of list
+ * Return: 0 if has no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
 	listint_t *tortoise, *hare;
 
-	/* If the list is empty isn't a cycle */
 	if (list == NULL)
 		return (0);
-	/* If the list has only one element isn't a cycle */
 	if (list->next == NULL)
 		return (0);
-	/* Initializing pointing to the first and the second */
+
 	tortoise = list;
 	hare = list->next;
-	/* Loop until find a loop or reach the end */
+
 	do {
-		/* Check for the loop */
 		if (tortoise == hare)
 			return (1);
-		/* Move the pointer on position */
 		tortoise = tortoise->next;
-		/* Move the pointer two positions, if possible */
 		if (hare->next != NULL)
 			hare = hare->next->next;
 		else
 			hare = NULL;
 	} while (tortoise != NULL && hare != NULL);
-	/* If we reach the end on the list isn't a cycle */
 	return (0);
 }
--- 0x07-linked_list_cycle/0-linked_lists.c
+++ 0x07-linked_list_cycle/0-linked_lists.c
@@ -3,29 +3,28 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of a listint_t list
+ * print_listint - prints all elements of list
  * @h: pointer to head of list
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *current;
-	unsigned int n; /* number of nodes */
-
+	unsigned int n;
 	current = h;
 	n = 0;
+
 	while (current != NULL)
 	{
 		printf("%i\n", current->n);
 		current = current->next;
 		n++;
 	}
-
 	return (n);
 }
 
 /**
- * add_nodeint - adds a new node at the beginning of a listint_t list
+ * add_nodeint - adds a new node
  * @head: pointer to a pointer of the start of the list
  * @n: integer to be included in node
  * Return: address of the new element or NULL if it fails
@@ -33,15 +32,13 @@ size_t print_listint(const listint_t *h)
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
-
 	new = malloc(sizeof(listint_t));
+
 	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
 	new->next = *head;
 	*head = new;
-
 	return (new);
 }
 
