d0dd593a8ca919c3b21dc77f12d36cc1217ab362
andres0191 <andres_felipe0191@hotmail.com>
2021-11-10 21:38:46 -0500

    update task

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,27 +1,25 @@
 #!/usr/bin/node
+// script that prints all characters of a Star Wars movie
 const request = require('request');
+const axios = require('axios');
 
-function send (person) {
-  return new Promise(function (resolve, reject) {
-    request(person, function (error, response, body) {
-      if (error || response.code >= 400) {
-        reject(error);
-      } else {
-        resolve(JSON.parse(body).name);
-      }
-    });
-  });
-}
+const endPoint = 'https://swapi-api.hbtn.io/api/films/';
+const filmNumber = process.argv[2] ? process.argv[2] : null;
+const url = endPoint + filmNumber;
 
-request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  async function (error, response, body) {
-    if (error) {
-      console.log(error);
-    } else {
-      const people = JSON.parse(body).characters;
-      for (let i = 0; i < people.length; ++i) {
-          const name = await send(people[i]);
-          console.log(name);
-      }
+request(url, async function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    body = JSON.parse(body);
+
+    for (const character in body.characters) {
+      await actor(body.characters[character]);
     }
-  });
+  }
+});
+
+async function actor (url) {
+  await axios.get(url)
+    .then(function (response) {
+      console.log(response.data.name);
+    });
+}
