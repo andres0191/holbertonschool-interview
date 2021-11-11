8dbe6bcdc91a3ccf876571516573ebb6205bffce
andres0191 <andres_felipe0191@hotmail.com>
2021-11-10 21:37:33 -0500

    update task

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,24 +1,27 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(url, { json: true }, async (err, response, body) => {
-  if (err) {
-    console.error(err);
-  } else {
-    const cast = body.characters;
+function send (person) {
+  return new Promise(function (resolve, reject) {
+    request(person, function (error, response, body) {
+      if (error || response.code >= 400) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body).name);
+      }
+    });
+  });
+}
 
-    for (const characterUrl of cast) {
-      const name = await new Promise((resolve, reject) => {
-        request(characterUrl, { json: true }, (err, response, body) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(body.name);
-          }
-        });
-      });
-      console.log(name);
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  async function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      const people = JSON.parse(body).characters;
+      for (let i = 0; i < people.length; ++i) {
+          const name = await send(people[i]);
+          console.log(name);
+      }
     }
-  }
-});
+  });
